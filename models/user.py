31f6cae47e4ba314d689d72597a05e2310cf7991
Katya Weicht <katya.weicht@gmail.com>
2025-07-12 21:49:27 -0600

    Validate name attribute

--- models/user.py
+++ models/user.py
@@ -54,3 +54,14 @@ class User:
     save_users()
     return new_user
   
+  @property
+  def name(self):
+    return self._name
+  
+  @name.setter
+  def name(self,value):
+    if not isinstance(value, str):
+            raise TypeError("Name must be a string")
+    if len(value) < 3:
+        raise ValueError("Name must be at least 3 characters long")
+    self._name = value
